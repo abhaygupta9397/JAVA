0af54e442c7e0a1fd6afc8016af2f58728348afe
Abhay Gupta <abhaygupta9397@gmail.com>
2025-08-13 22:55:00 +0530

    Initial commit

---
+++ Java-fundamentals/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+    public static void main(String args[]){
+        System.out.println("Abhay is here hello world from him");
+        System.out.println("hello from me is well");
+    }
+}
